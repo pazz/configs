c4f40b5776695369222b3734e30d88d1628f191d
patrick <p.totzke@ed.ac.uk>
2011-04-11 20:22:11 +0100

    offlineimap config

--- .offlineimap.py
+++ .offlineimap.py
@@ -4,6 +4,6 @@ import re, os
 def get_authinfo_password(machine, login, port):
     s = "machine %s login %s password ([^ ]*) port %s" % (machine, login, port)
     p = re.compile(s)
-    authinfo = os.popen("gpg -q --no-tty -d ~/.authinfo.gpg").read()
+    authinfo = os.popen("gpg -q --use-agent --no-tty -d ~/.authinfo.gpg").read()
     #authinfo = os.popen("gpg -q -d ~/.authinfo.gpg").read()
     return p.search(authinfo).group(1)
