697185591e648b25302b56f05ec2e7ebc5997f8c
Patrick Totzke <patricktotzke@gmail.com>
2012-03-02 08:51:24 +0000

    new alot config syntax

--- .config/alot/hooks.py
+++ .config/alot/hooks.py
@@ -1,6 +1,7 @@
 import logging
-def pre_exit(aman=None, **rest):
-    accounts = aman.get_accounts()
+from alot.settings import settings
+def pre_global_exit(**args):
+    accounts = settings.get_accounts()
     if accounts:
         logging.info('goodbye, %s!' % accounts[0].realname)
     else:
