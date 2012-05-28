9ffdb805c1b16ab5335b4677323ea50e3e6dcd68
patrick <p.totzke@ed.ac.uk>
2012-05-28 10:00:39 +0100

    rm old configs

--- .alot.py
+++
@@ -1,6 +0,0 @@
-def pre_exit(ui, dbman, accountman, config):
-    accounts = accountman.get_accounts()
-    if accounts:
-        ui.logger.info('goodbye, %s!' % accounts[0].realname)
-    else:
-        ui.logger.info('goodbye!')
